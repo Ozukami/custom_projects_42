b70e4ee552901b09a82b4d34635b7c235ccf451c
Arthur POISSON <apoisson@e1r3p6.42.fr>
2017-03-28 23:09:56 +0200

    libft a jour

---
+++ filler_v2/libft/ft_count_digit.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_digit.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apoisson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/01 03:15:15 by apoisson          #+#    #+#             */
+/*   Updated: 2017/03/01 04:04:03 by apoisson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+int		ft_count_digit(long value)
+{
+	if (value > -10 && value <= -1)
+		return (2);
+	if (value >= 0 && value < 10)
+		return (1);
+	if (value < -10)
+		return (2 + ft_count_digit(-value / 10));
+	return (1 + ft_count_digit(value / 10));
+}
